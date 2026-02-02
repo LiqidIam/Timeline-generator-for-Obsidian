848903ed32cabd2f9c13ccbb2be5fa1b73afe446
LiqidIam <mr.liqid@yandex.ru>
2026-02-02 21:54:40 +0300

    final test

--- JSON gen.py
+++ JSON gen.py
@@ -34,6 +34,7 @@ def generate_timeline(start_year=1810, end_year=2100, step=10, width_per_year=25
 	return json.dumps(canvas, indent=2, ensure_ascii=False)
 
 # Использование
+# print(generate_timeline())
 
 # Сохранить в файл .canvas
 with open("timeline.canvas", "w", encoding="utf-8") as f:
